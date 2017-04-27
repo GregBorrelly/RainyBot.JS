cb9d5c4401fe210cc327c0650f4dd2524fce7441
backslash092 <gregborrelly@gmail.com>
2017-04-27 08:33:36 -0400

    Works great

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ angular.module('umbrellaAlert', []);
 angular.module('umbrellaAlert')
     .controller('mainCrtl', ['$scope', '$http', function ($scope, $http) {
         $scope.geolocationLoaded = false;
+        
 
-        $scope.callWeatherAPI = function (latitude, longitude) {
+        var callWeatherAPI = function (latitude, longitude) {
             console.log("This ran");
             var apiKey = "324cb7915f1b85e43f1bfb17f861a027";
             var apiUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
@@ -16,76 +17,52 @@ angular.module('umbrellaAlert')
 
         };
 
-   
+        var geoLocation = {
 
-        $scope.getLocation = function () {
-            if (navigator.geolocation) {
-                console.log(1)
-                navigator.geolocation.getCurrentPosition(function(position){
-                      console.log(2)
-                     var  positionObject = {latitude:25, longitude:35}; 
-                      return positionObject;
-                });
-                 
+    getLocation: function() {
 
+        var deferred = $.Deferred();
 
+        // if geo location is supported
+        if(navigator.geolocation) {
 
+            // get current position and pass the results to getPostalCode or time out after 5 seconds if it fails
+            navigator.geolocation.getCurrentPosition(deferred.resolve, this.geoLocationError, {
+                timeout: 5000
+            });
 
-            } else {
+        } else {
 
-
-                console.log("GEOLOCATION IS NOT SUPPORTED BY THE BROWSER.")
-            }
-        };
-
-        $scope.initializeApp = function () {
-        
-
-       
-            var position= $scope.getLocation();
-            while (position == undefined){
-                console.log('getting object...');
-                 
-                
-          
-
-            }
-
-              console.log(position);
-
-            }
-           
-           
-    
-        
-     };
-
-        
-
-
-    
-        // number of drops created.
-        var nbDrop = 10; 
-
-        // function to generate a random number range.
-        function randRange( minNum, maxNum) {
-        return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+            //geo location isn't supported
+            console.log('Your browser does not support Geo Location.');
         }
 
-        // function to generate drops
-        $scope.createRain = function() {
+        return deferred.promise();
 
-            for( i=1;i<nbDrop;i++) {
-            var dropLeft = randRange(0,1600);
-            var dropTop = randRange(-100,400);
+    },
 
-            $('.rain').append('<div class="drop" id="drop'+i+'"></div>');
-            $('#drop'+i).css('left',dropLeft);
-            $('#drop'+i).css('top',dropTop);
-            }
-
+    geoLocationError: function() {
+        console.log('Geo Location failed.');
     }
 
+};
+        $scope.initializeApp = function () {
+            $.when(geoLocation.getLocation()).then(function(data, textStatus, jqXHR) {
+
+                $scope.cordinates = {longitude:data.coords.longitude, latitude:data.coords.latitude};
+                console.log($scope.cordinates);
+                $scope.theLink = callWeatherAPI( data.coords.latitude, data.coords.longitude);
+                 
+                var req = new XMLHttpRequest();
+                req.open("GET", 'http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=626020384932586fe3fdaafc11b18c48&units=imperial', false);
+                req.send(null);
+                console.log(req.status, req.responseText)
+                $scope.globalRes = req.responseText;
+
+            });
+
+            console.log($scope.theLink);
         
+            };
 
     }]);
