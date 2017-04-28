651793daa25853edadb963780740d6dfe82b6e6e
backslash092 <gregborrelly@gmail.com>
2017-04-28 02:14:48 -0400

    It worksgit add .git add .

--- main.js
+++ main.js
@@ -1,68 +1,57 @@
-angular.module('umbrellaAlert', []);
+
 
 angular.module('umbrellaAlert')
-    .controller('mainCrtl', ['$scope', '$http', function ($scope, $http) {
-        $scope.geolocationLoaded = false;
-        
+    .controller('mainCrtl', ['$scope', function ($scope) {
+       
+        $scope.why = false;
 
-        var callWeatherAPI = function (latitude, longitude) {
-            console.log("This ran");
-            var apiKey = "324cb7915f1b85e43f1bfb17f861a027";
-            var apiUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey + "&units=imperial";
-            console.log(apiUrl);
-            return apiUrl;
-
+        function get(url){
+            return new Promise(function(succeed, fail){
+                var req = new XMLHttpRequest();
+                req.open("GET", url, true);
+                req.addEventListener("load", function(){
 
 
-
-        };
-
-        var geoLocation = {
-
-    getLocation: function() {
-
-        var deferred = $.Deferred();
-
-        // if geo location is supported
-        if(navigator.geolocation) {
-
-            // get current position and pass the results to getPostalCode or time out after 5 seconds if it fails
-            navigator.geolocation.getCurrentPosition(deferred.resolve, this.geoLocationError, {
-                timeout: 5000
+                    if (req.status < 400)
+                        succeed(JSON.parse(req.responseText));
+                    
+                    else
+                        fail(new Error("Request FAiled: " + req.statusText));
+                });
+                req.addEventListener("error", function(){
+                    fail(new Error("Network error"));    
+                 });
+                req.send(null);
             });
-
-        } else {
-
-            //geo location isn't supported
-            console.log('Your browser does not support Geo Location.');
         }
 
-        return deferred.promise();
 
-    },
+        if ("geolocation" in navigator) {
+             /* geolocation is available */
+             navigator.geolocation.getCurrentPosition(function(position) {
+            var latitude = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            var API = "http://api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&appid=626020384932586fe3fdaafc11b18c48&units=imperial";
+           
+            get(API).then(function(response){
+                $scope.$apply(function(){
 
-    geoLocationError: function() {
-        console.log('Geo Location failed.');
-    }
+                    console.log(response);
+                    $scope.data = response;
+                    $scope.why = true;
+                    $scope.today = Math.round($scope.data.list[0]["main"]["temp"]) + " F"; 
 
-};
-        $scope.initializeApp = function () {
-            $.when(geoLocation.getLocation()).then(function(data, textStatus, jqXHR) {
-
-                $scope.cordinates = {longitude:data.coords.longitude, latitude:data.coords.latitude};
-                console.log($scope.cordinates);
-                $scope.theLink = callWeatherAPI( data.coords.latitude, data.coords.longitude);
-                 
-                var req = new XMLHttpRequest();
-                req.open("GET", 'http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=626020384932586fe3fdaafc11b18c48&units=imperial', false);
-                req.send(null);
-                console.log(req.status, req.responseText)
-                $scope.globalRes = req.responseText;
+                    console.log($scope.why);
+                    console.log($scope.latitude);
 
             });
+           
+         });
+        });
+        } else {
+            /* geolocation IS NOT available */
+        }
 
-            console.log($scope.theLink);
-        
-            };
+     
 
     }]);
